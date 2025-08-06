dd56eba01e49adb23bf496254827d6ef07fca523
mani2005-urs <mail2maxi16@gmail.com>
2025-08-06 19:27:31 +0530

    Create Leetcode1.java
    1. Two Sum
    Solved
    Easy
    Topics
    premium lock icon
    Companies
    Hint
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.
    
     
    
    Example 1:
    
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
    Example 2:
    
    Input: nums = [3,2,4], target = 6
    Output: [1,2]
    Example 3:
    
    Input: nums = [3,3], target = 6
    Output: [0,1]
     
    
    Constraints:
    
    2 <= nums.length <= 104
    -109 <= nums[i] <= 109
    -109 <= target <= 109
    Only one valid answer exists.
     
    
    Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?

---
+++ Leetcode1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr=new int[2];
+        int i,j;
+        for(i=0;i<nums.length;i++)
+        {
+            for(j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    arr[0]=i;
+                    arr[1]=j;
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+}
